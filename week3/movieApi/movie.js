1ba58742132ec011e7ada623b5f63a98c13e9c64
Crystal Mulluns <stardustCrystal@users.noreply.github.com>
2020-06-15 21:24:36 -0700

    P

--- week3/movieApi/movie.js
+++ week3/movieApi/movie.js
@@ -6,11 +6,11 @@ app.use(express.json())
 
 
 const bountys = [
-    {firstName:"Matt", lastName:"Madd", living: true, bounty_payout: 4000, type: "Sith", _id:uuidv4()},
+    {firstName:"Matt", lastName:"Madd", living: false, bounty_payout: 4000, type: "Sith", _id:uuidv4()},
     {firstName:"John", lastName:"Mayer", living: true, bounty_payout: 45000, type: "Jedi", _id:uuidv4()},
     {firstName:"Ruth", lastName:"Babe", living: true, bounty_payout: 55000, type: "Sith", _id:uuidv4()},
     {firstName:"Crystal", lastName:"Mayer", living: true, bounty_payout: 45000, type: "Jedi", _id:uuidv4()},
-    {firstName:"Mag", lastName:"Azine", living: true, bounty_payout: 65000, type: "Sith", _id:uuidv4()},
+    {firstName:"Mag", lastName:"Azine", living: false, bounty_payout: 65000, type: "Sith", _id:uuidv4()},
 
 ]
 
