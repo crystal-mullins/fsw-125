f95ae5911f75045b62513cc3bbf3327e1dbe41f7
Crystal Mulluns <stardustCrystal@users.noreply.github.com>
2020-07-08 22:07:04 -0700

    P

--- week6/JMSongServer/jmsongrouter.js
+++ week6/JMSongServer/jmsongrouter.js
@@ -11,59 +11,59 @@ jmSongRouter.use(morgan('dev'))
 const songlist = [
 
     {
-        order: "A",
+        type: "A",
         songs: "Astrothunder",
         _id: uuidv4()
     },{
-        order: "B",
+        type: "B",
         songs: "Belief, Bigger Than My Body",
         _id: uuidv4()
     },{
-        order: "C",
+        type: "C",
         songs: "Clarity ",
         _id: uuidv4()
     },{
-        order: "D",
+        type: "D",
         songs: "Daughters"
         ,_id: uuidv4()
     },{
-        order: "G",
+        type: "G",
         songs: "Gravity ",
         _id: uuidv4()
     },{
-        order: "H",
+        type: "H",
         songs:"Half of My Heart, The Heart of Life, Heartbreak Warfare",
         _id: uuidv4()
     },{
-        order: "I",
+        type: "I",
         songs: "I Don't Trust Myself (With Loving You), I'm Gonna Find Another You, In Repair, In the Blood",
         _id: uuidv4()
     },{
-        order: "L",
+        type: "L",
         songs: "Love on the Weekend",
         _id: uuidv4()
     },{
-        order: "N",
+        type: "N",
         songs:"Neon, New Light, No Such Thing ",
         _id: uuidv4()
     },{
-        order: "Q",
+        type: "Q",
         songs: "Queen of California",
         _id: uuidv4()
     },{
-        order: "S",
+        type: "S",
         songs:"Say (song), Shadow Days, Slow Dancing in a Burning Room, Small Worlds (song), Something's Missing, Stop This Train",
         _id: uuidv4()
     },{
-         order: "V",   
+         type: "V",   
         songs: "Vultures (song)",
         _id: uuidv4()
     },{
-        order: "W",
+        type: "W",
         songs: "Waiting on the World to Change, Who Did You Think I Was, Who Says, Who You Love, Why Georgia",
         _id: uuidv4()
     },{
-        order: "Y",
+        type: "Y",
         songs: "Your Body Is a Wonderland",
         _id: uuidv4()
     }
@@ -80,7 +80,7 @@ jmSongRouter.get("/", (req, res) => {
 jmSongRouter.get("/:songlistId", (req, res, next) => {
     const songlistId = req.params.songlistId
     const foundSong = songlist.find(songlist => songlist._id === songlistId)
-    if(!foundsong){
+    if(!foundSong){
         const error = new Error("500 this is not the song your looking for!")
         res.status(500)
         return next(error)
@@ -88,17 +88,18 @@ jmSongRouter.get("/:songlistId", (req, res, next) => {
     res.status(200).send(foundSong)
 })
 
-// get by key
+// get by key value
 
-jmSongRouter.get("/search/order", (req, res, next) => {
-    const order = req.params.order
-    if(!order){
+jmSongRouter.get("/search/type", (req, res, next) => {
+    const type = req.params.type
+    const filteredSongs = songlist.filter(songlist => songlist.type === type)
+    if(!type){
         const error = new Error("You must provide letter")
         res.status(500)
         return next(error)
     }
-    const filteredsongs = songlist.filter(songlist => songlist.order === order)
-    res.status(200).send(filteredsongs)
+    
+    res.status(200).send(filteredSongs)
 })
 
 jmSongRouter.post("/", (req, res) => {
@@ -114,7 +115,7 @@ jmSongRouter.post("/", (req, res) => {
     const newSong = req.body
     newSong._id = uuidv4()
     songlist.push(newSong)
-    res.send(`Successfully added ${newSong.order} to the database`)
+    res.send(`Successfully added ${newSong.type} to the database`)
 
 })
 // delete one
@@ -122,7 +123,7 @@ jmSongRouter.delete("/:songlistId", (req, res) => {
     const songlistId = req.params.songlistId
     const songIndex = songlist.findIndex(songlist => songlist._id === songlistId)
     songlist.splice(songIndex, 1)
-    res.send("Successfully delete bounty")
+    res.send("Successfully delete songs")
 })
 
 //  update one
