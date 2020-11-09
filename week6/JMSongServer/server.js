1f8d256503e1b3f4ec6314b1be1a88b0e62c4f95
Crystal Mulluns <stardustCrystal@users.noreply.github.com>
2020-11-08 20:52:13 -0800

    P

--- week6/JMSongServer/server.js
+++ week6/JMSongServer/server.js
@@ -1,6 +1,40 @@
 var express = require("express");
 const app = express();
- app.use(express.json())
+ app.use(express.json());
+ const mysql = require('mysql');
+
+
+ var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : 'secret'
+});
+ 
+connection.connect(function(err) {
+  console.log(err)
+  if (err) {
+    console.error('error connecting: ');
+    return;
+  }
+ 
+  console.log('connected as id ' + connection.threadId);
+});
+
+// const con = mysql.createConnection({
+//   host: "localhost",
+//   user: "root",
+//   password: "secret",
+//   database: 'Songlist'
+// });
+
+// con.connect(function(err) {
+//   if (err) throw err;
+//   con.query("SELECT * FROM Songlist", function (err, result, fields) {
+//     if (err) throw err;
+//     console.log(result);
+//   });
+// });
+
 
 
 app.use("/songlist/", require("./jmsongrouter.js"));
